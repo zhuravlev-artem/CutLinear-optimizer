ed9bc57039465fa614e6c27b24cef447c52fc034
Artemisimus <ar.a.zhuravlev@gmail.com>
2023-11-06 23:34:59 +0300

    The second stable verson with a greedy algorithm

--- optimizer-1.2.c
+++ optimizer-1.2.c
@@ -1,214 +1,346 @@
 #include <stdio.h>
 #include <windows.h>
 #include <stdbool.h>
+#include <locale.h>
+
+#define lenght_parts 11
+#define lenght_boards 6
+#define EBUG_MODE 0
+#define SEND_MODE 0
+
+/*
+
+*/
+
+struct
+{
+    int length;
+    bool used;
+} part[11]= {
+    {0, 1},
+    {1, 1},
+    {4, 1},
+    {7, 1},
+    {2, 1},
+    {4, 1},
+    {8, 1},
+    {1, 1},
+    {10, 1},
+    {5, 1},
+    {6, 1},
+};
+
+struct
+{
+    int length;
+    int part_counter;
+    int old_remnat;
+    bool used;
+    int combination[11];
+    int parts_buffer[11];
+    int best_combination[11];
+} board[6]={
+    {10, 0, 10000, 1, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    {2, 0, 10000, 1, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    {4, 0, 10000, 1, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    {16, 0, 10000, 1, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    {8, 0, 10000, 1, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    {6, 0, 10000, 1, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+};
+
+int disk_thickness=3;
 
 
-int combination[1000] [1000];                        //доска и номер детали
-int best_combination[1000] [1000];                   //лучшее расположение
-int old_remnat=10000;
-int board[1000];
-int part[1000];
-int part_count[1000];              //кол-во деталей на одной доске*/
-int lenght_boards=0;
-int lenght_parts=0;
-int disk_thickness=0;
-int old_take_boards=1000;
 
-void inputs(){
-    for(int i=0; i<1000; i++){
-        part_count[i]=0;
-        board[i]=0;
-        part[i]=0;
-    }
-    lenght_boards=0;
-    lenght_parts=0;
-    disk_thickness=0;
+void dprint()
+{
 
-    int i=0;
-    while(1){
-        int scan4=1;
-        printf("доска №%d:", i+1);
-        scanf("%d", &scan4);
-        if(scan4==0){break;}
-        board[i]=scan4;
-        lenght_boards=i+2;
-        i++;
-    }
-    board[lenght_boards-1]=10000;
-    printf("\n");
-
-    i=1;
-    part[0]=0;
-    while(1){
-        int scan4=1;
-        printf("деталь №%d:", i);
-        scanf("%d", &scan4);
-        if(scan4==0){break;}
-        lenght_parts=i+1;
-        part[i]=scan4;
-        i++;
-    }
-
-    printf("\nтолщина пилы:");
-    scanf("%d", &disk_thickness);
-    printf("\n");
-}
-
-
-void init(){                                    //инициализация нулями
-    for(int i=0; i<lenght_boards; i++){
-        for(int j=0; j<lenght_parts; j++){
-            best_combination[i] [j]=0;
-            combination[i] [j]=0;
+    printf("основная раскладка:\n");
+    for(int i=0; i<6; i++)
+    {
+        printf("(%d):\t", board[i].length);
+        for(int j=0; j<11; j++)
+        {
+            printf("%d;\t", part[board[i].combination[j]].length);
         }
+        printf("обрезок: %d\n", board[i].old_remnat);
     }
+    printf("\n");
+
+
+    printf("буфер:\n");
+    for(int i=0; i<6; i++)
+    {
+        printf("(%d):\t", board[i].length);
+        for(int j=0; j<11; j++)
+        {
+            printf("%d;\t", part[board[i].parts_buffer[j]].length);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+
+    printf("итог:\n");
+    for(int i=0; i<6; i++)
+    {
+        printf("(%d):\t", board[i].length);
+
+        for(int j=0; j<11; j++)
+        {
+            printf("%d;\t", part[board[i].best_combination[j]].length);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    printf("используются:\t");
+    for(int i=1; i<11; i++)
+    {
+        printf("%d:%d; ", part[i].length, part[i].used);
+    }
+    printf("\n");
+
+    system("pause");
 }
 
 
-int summ_length_parts(int b){                   //какой длинны все установленные на доске детаи?
+/*
+пробежаться по всем позициям, сложить длины деталей под индексами позиции
+с учётом полщины диска
+*/
+int summ_length_parts(int b)
+{
     int buff=0;
-    for(int i=0; i<lenght_parts; i++){
-        buff += part[combination[b][i]];
-        if(i>0&&combination[b][i]!=0){
+    for(int i=0; i < lenght_parts; i++)
+    {
+
+        buff += part[board[b].combination[i]].length;
+
+        if(i > 0 && board[b].combination[i] != 0)
+        {
             buff += disk_thickness;
         }
     }
-return buff;
+    return buff;
 }
 
-
-bool can_place_part(int p, int b){              //можем ли мы расположить деталь на доску?
+/*
+если длинна всех деталей на доске вместе с новой деталью
+не больше длинны доски, вернуть да
+*/
+bool can_place_part(int p, int b)
+{
     int d, f=summ_length_parts(b);
-    if(f==0){
-        d=0;
-    }else{
-        d=1;
+
+    if(f == 0)
+    {
+        d = part[p].length;
     }
-    if(f + part[p] + disk_thickness*d <=board[b]){
-        return TRUE;
-    } else{
-        return FALSE;
+    else
+    {
+        d = f + part[p].length + disk_thickness;
     }
-}
 
-
-void place_part(int p, int b){                  //расположить доску
-    combination[b] [part_count[b]] = p;
-    part_count[b]++;
-}
-
-
-void remove_part(int b){                        //удалить доску
-    if(combination[b] [0]!=0){
-        part_count[b]--;
-        combination[b] [part_count[b]]=0;
+    if(d <= board[b].length)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
 
-
-void copy_best_combination(){                   //скопировать лучшую комбинацию и еë параметры
-    for(int i=0; i<lenght_boards; i++){
-        for(int j=0; j<lenght_parts+1; j++){
-            best_combination[i] [j] = combination[i] [j];
-        }
-    }
-old_take_boards = take_boards();
-old_remnat = summ_length_parts(lenght_boards-1);  //запомнить суммарную длинну дкталей на мнимой доске
-printmass(0);
+/*
+поставить деталь на доску, поставить счётчик на следующиц индекс
+*/
+void place_part(int p, int b)
+{
+    board[b].combination[board[b].part_counter] = p;
+    board[b].part_counter++;
 }
 
-
-int take_boards(){
-    int buff=0;
-    for(int i=0; i<lenght_boards-1; i++){
-        if(combination[i][0]!=0){buff++;}
-    }
-return buff;
-}
-
-
-void check(){                                   //проверка на оптимальность
-    int a=summ_length_parts(lenght_boards-1);   //остаток на мнимой доске
-    if(old_remnat>a){                           //если суммарная длинна деталей на мнимой доске меньше, чем в последний раз, то
-        copy_best_combination();                //запомнить комбинацию
-    }else if(old_remnat==a && take_boards()<old_take_boards){
-        copy_best_combination();
+/*
+декрементировать счётчик, обнулить позицию
+*/
+void remove_part(int b)
+{
+    if(board[b].combination[0]!=0)
+    {
+        board[b].part_counter--;
+        board[b].combination[board[b].part_counter]=0;
     }
 }
 
+/*
+пробежаться по всем доскам, выбрать самый маленький обрезок
+*/
+int shorter_remnat()
+{
+    int min = board[0].old_remnat, b=0;
 
-void recurs(int p){                             //рекурсивный проход и проверка на оптимальность
-    if(p<lenght_parts){                         //если переданная деталь не последняя, то
-        for(int i=0; i<lenght_boards; i++){     //на каждой доске
-            if(can_place_part(p, i)){           //попытаться расположить деталь
-                place_part(p, i);               //если вмещается, то расположить, затем
-                recurs(p+1);                    //вызвать этот же цикл, передать следующую деталь
-                remove_part(i);                 //затем убрать последнюю деталь
+    for(int i=0; i<lenght_boards; i++)
+    {
+
+        if(board[i].used != 0)
+        {
+            if(min > board[i].old_remnat)
+            {
+
+                min = board[i].old_remnat;
+                b = i;
             }
         }
-    }else{                                      //если передаваемой детали не существует, то
-        check();                                //сделать проверку на оптимальность
+    }
+    return b;
+}
+
+/*
+сбросить рабочую и буферную позицию, счётчик и обрезки
+*/
+void clear()
+{
+    for(int i=0; i<lenght_boards; i++)
+    {
+        for(int j=0; j<lenght_parts; j++)
+        {
+            board[i].combination[j]=0;
+            board[i].parts_buffer[j]=0;
+            board[i].part_counter=0;
+        }
+        board[i].old_remnat=10000;
+    }
+}
+
+/*
+ообновить значение обрезка, скопировать в буффер
+рабочую комбинацию
+*/
+void copy_to_buffer(int b)
+{
+
+    board[b].old_remnat = board[b].length - summ_length_parts(b);
+
+    for(int i=0; i<lenght_parts; i++)
+    {
+        board[b].parts_buffer[i] = board[b].combination[i];
+    }
+}
+
+/*
+
+*/
+void check(int b)
+{
+    if(board[b].old_remnat > board[b].length - summ_length_parts(b))
+    {
+        copy_to_buffer(b);
     }
 }
 
 
-void printmass(int a){
-if(a==0){
+void recurs(int n, int b)
+{
+    bool flag=1;
+    for(int i=n; i<lenght_parts; i++)
+    {
 
-    for(int i=0; i<lenght_boards-1; i++){
-        printf("(%d):", board[i]);
+        if((part[i].used != 0))
+        {
 
-        for(int j=0; j<lenght_parts; j++){
-            if(part[best_combination[i][j]]!=0){
-                printf(" (№%d)%d", best_combination[i][j], part[best_combination[i][j]]);
+            if(can_place_part(i, b))
+            {
+                flag=1;
+                place_part(i, b);
+
+#if EBUG_MODE
+                dprint();
+#endif // EBUG_MODE
+
+                recurs(n+1, b);
+                remove_part(b);
             }
+            else if(flag==1)
+            {
+                check(b);
+
+#if EBUG_MODE
+                dprint();
+#endif // EBUG_MODE
+
+                flag=0;
+            }
+
+        }
+
+    }
+}
+
+
+void copy_to_fin(int b)
+{
+    for(int i=0; i<lenght_parts; i++)
+    {
+        board[b].best_combination[i] = board[b].parts_buffer[i];
+
+        part[board[b].best_combination[i]].used=0;
+    }
+    board[b].used=0;
+}
+
+
+void run()
+{
+    for(int i=0; i<lenght_boards; i++)
+    {
+        if(board[i].used != 0)
+        {
+            for(int j=0; j<lenght_boards; j++)
+            {
+                recurs(1, j);
+            }
+            copy_to_fin(shorter_remnat());
+            clear();
+        }
+    }
+}
+
+
+void printmass()
+{
+    for(int i=0; i<lenght_boards; i++)
+    {
+        printf("Доска №%d (%d):", i, board[i].length);
+        for(int j=0; j<lenght_parts; j++)
+        {
+            if(board[i].best_combination[j]!=0)
+                printf(" %d;", part[board[i].best_combination[j]].length);
         }
         printf("\n");
     }
-    printf("(inf):");
 
-    for(int j=0; j<lenght_parts; j++){
-        if(part[best_combination[lenght_boards-1][j]]!=0){
-            printf(" (№%d)%d", best_combination[lenght_boards-1][j], part[best_combination[lenght_boards-1][j]]);
+    printf("Невместившиеся детали:");
+    for(int j=0; j<lenght_parts; j++)
+    {
+        if(part[j].used != 0)
+        {
+            printf(" %d;", part[j].length);
         }
-    }
-    printf("\n\n");
-}
-
-else if(a==1){
-
-    for(int i=0; i<lenght_boards; i++){
-
-        if(i<lenght_boards-1){
-            printf("(%d):", board[i]);
-        }else{printf("(inf):");}
-
-        for(int j=0; j<lenght_parts; j++){
-            printf("%d; ", part[combination[i][j]]);
-        }
-        printf("\n");
-        if(i<lenght_boards-1){
-            printf("(%d):", board[i]);
-        } else{
-            printf("(inf):");
-        }
-        for(int k=0; k<part_count[i]; k++){
-            printf("   ");
-        }
-    printf("^ %d", part_count[i]);
-    printf("\n");
     }
     printf("\n");
 }
 
-}
 
-
-int main(){
-    SetConsoleOutputCP(CP_UTF8);
-    init();
-    inputs();
-    recurs(1);
+int main()
+{
+    #if SEND_MODE
+        setlocale(CP_UTF8, "russian");
+    #else
+        SetConsoleOutputCP(CP_UTF8);
+    #endif
+    run();
+    printmass();
     system("pause");
-return 0;
+    return 0;
 }
